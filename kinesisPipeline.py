639979e72f88b167fa27a3ea55f931d52fa3dcaf
maddy <maddy@makeskilled.com>
2023-04-10 20:25:02 +0530

    updating api

--- kinesisPipeline.py
+++ kinesisPipeline.py
@@ -2,12 +2,15 @@ import dataSource as KinesisDataStream
 import paho.mqtt.client as mqtt
 import time
 
-kinesisServer='172.31.80.42'
+kinesisServer='broker.hivemq.com'
+client=mqtt.Client()
+
 def send_to_kinesisCluster(data):
-    client=mqtt.Client()
     client.connect(kinesisServer,1883)
-    client.publish('b17/data',data)
+    print('Connected with Kinesis Server')
+    client.publish('b17/aws',data,0)
     print('Data Published Successfully')
+    client.disconnect()
 
 while True:
     dataStream=KinesisDataStream.data_healthBody()
--- kinesisPlug.py
+++ kinesisPlug.py
@@ -1,9 +1,10 @@
 import paho.mqtt.client as mqtt
 
-kinesisServer='172.31.80.42'
+kinesisServer='broker.hivemq.com'
 client=mqtt.Client()
 client.connect(kinesisServer,1883)
-client.subscribe('b17')
+print('Connected with Kinesis Server')
+client.subscribe('b17/aws')
 
 def notification(client,userdata,msg):
     print(msg.payload)

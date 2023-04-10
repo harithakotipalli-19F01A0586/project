6323bd0351e1604102b2ece85fab43b788b80b2e
maddy <maddy@makeskilled.com>
2023-04-10 20:29:58 +0530

    updated the json object

--- kinesisPlug.py
+++ kinesisPlug.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import json
 
 kinesisServer='broker.hivemq.com'
 client=mqtt.Client()
@@ -7,7 +8,17 @@ print('Connected with Kinesis Server')
 client.subscribe('b17/aws')
 
 def notification(client,userdata,msg):
-    print(msg.payload)
+    data=(msg.payload)
+    print(data)
+    data=data.decode('utf-8')
+    data=json.loads(data)
+    pulseRate=data['pulseRate']
+    spo2=data['spo2']
+    bpm=data['bpm']
+    glucose=data['glucose']
+    hemoglobin=data['hemoglobin']
+    bodytemp=data['bodytemp']
+    print(pulseRate,spo2,bpm,glucose,hemoglobin,bodytemp)
 
 client.on_message=notification
 client.loop_forever()
